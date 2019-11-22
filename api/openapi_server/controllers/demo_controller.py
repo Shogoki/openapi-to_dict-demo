b54b5bc2e84e1523994c7f87520dd4d99445a4bb
Sven Kraus <mail@svenkraus.de>
2019-11-22 19:08:17 +0100

    added LOG outputs as comments

--- api/openapi_server/controllers/demo_controller.py
+++ api/openapi_server/controllers/demo_controller.py
@@ -21,13 +21,22 @@ def get_demo():  # noqa: E501
     model = MyModel(demo_name="name", id=1)
     # Property has snake_case Name: demoName --> demo_name
     log.warn("Model is: {}".format(model))
+    # OUTPUT: WARNING in demo_controller: Model is: {'demo_name': 'name', 'id': 1}
 
     # using to_dict method to get a dict from Model
     model_dict = model.to_dict()
     log.warn("Model.to_dict() is: {}".format(model_dict))
+    # OUTPUT: WARNING in demo_controller: Model.to_dict() is: {'demo_name': 'name', 'id': 1}
 
     # now we want to convert that dict (probably fetched from DB) back to our Model
     new_model = MyModel.from_dict(model_dict)
     log.warn("New Created Model is: {}".format(new_model))
+    # OUTPUT: WARNING in demo_controller: New Created Model is: {'demo_name': None, 'id': 1}
+    # --> demo_name = None !!!!
 
     return new_model, 200
+
+# APP output when running GET /demo
+# WARNING in demo_controller: Model is: {'demo_name': 'name', 'id': 1}
+# WARNING in demo_controller: Model.to_dict() is: {'demo_name': 'name', 'id': 1}
+# WARNING in demo_controller: New Created Model is: {'demo_name': None, 'id': 1}
